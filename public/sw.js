af8e3f043724be3e7264fedcaf11fbd1550c5374
jlvFlores <jluisvazquez.jv@gmail.com>
2022-10-22 14:59:28 -0500

    126. Opciones de una notificación

--- public/sw.js
+++ public/sw.js
@@ -136,10 +136,21 @@ self.addEventListener('sync', e => {
 self.addEventListener('push', e => {
 
     // console.log(e);
-    console.log(e.data.text());
+    
+    const data = JSON.parse( e.data.text() );
+    
+    console.log(data);
 
     const title = data.titulo;
-    const options = {};
+    const options = {
+        body: data.cuerpo,
+        // icon: 'img/icons/icon-72x72.png',
+        icon: `img/avatars/${ data.usuario }.jpg`,
+        badge: 'img/favicon.ico',
+        image: 'https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/5/5b/Torre_de_los_Avengers.png/revision/latest?cb=20150626220613&path-prefix=es',
+        vibrate: [125,75,125,275,200,275,125,75,125,275,200,600,200,600],
+        openUrl: '/'
+        };
 
     e.waitUntil( self.registration.showNotification(title, options) );
 
--- server/push.js
+++ server/push.js
@@ -29,7 +29,7 @@ module.exports.sendPush = (post) => {
 
     suscripciones.forEach( (suscripcion, i) => {
 
-        webpush.sendNotification( suscripcion , post.titulo );
+        webpush.sendNotification( suscripcion , JSON.stringify( post ) );
 
     });
 
